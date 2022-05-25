70fb8648c3ee80ef86d3e06ffc2f5ae32b04ba19
profile1 <a.kotynski@hotmail.com>
2022-05-25 05:31:50 +0200

    Add project files.

---
+++ Loops/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myInt = 2;
+
+            var userInput1 = Console.ReadLine();
+            //Console.WriteLine(userInput1);
+
+            while (myInt < 10)
+            {
+                myInt += 1;
+            }
+            Console.WriteLine(myInt);
+            for (int i = 0; i < 10; i++)
+            {
+
+            }
+
+        }
+    }
+}
+
+
+
+// pętla while wykonywana jest do momentu wykonania warunku
+// debugger (czerwona kropka) press F10 for next line
+// 'i++' to jest to samo co 'i += 1'
+
+// 'for' loop structure:          for (variable i set at 0; length of said loop; set increment)
